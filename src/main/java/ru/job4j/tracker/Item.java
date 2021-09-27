64cb2041800ffcb2ca1becde72736bf22caac58b
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-09-27 16:11:11 +0300

    2. Перегрузка конструктора [#173890][2]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,6 +5,17 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
     public int getId() {
         return id;
     }
@@ -19,17 +30,5 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Item() {
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
-        this.name = name;
-        this.id = id;
     }
 }
