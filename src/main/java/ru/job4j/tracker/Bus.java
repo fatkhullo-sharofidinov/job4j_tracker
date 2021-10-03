6222eff6f0e2d9cebb50d59b837b1ebc6280d651
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-10-03 22:17:15 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        System.out.println("Transport is drive.");
+    }
+
+    @Override
+    public void passengers(int count) {
+        System.out.println("The number of passengers: " + count);
+    }
+
+    @Override
+    public int refuel(int liters) {
+        return liters * 50;
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker;
+
+public interface Transport {
+    void drive();
+    void passengers(int count);
+    int refuel(int liters);
+}
