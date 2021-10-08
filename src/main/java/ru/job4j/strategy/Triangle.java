e4d9d54b5be146327071bade54e545a887dc3177
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-10-08 14:41:06 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]  [2]

--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -4,9 +4,10 @@ public class Triangle implements Shape {
 
     @Override
     public String draw() {
+
         String ln = System.lineSeparator();
         return    "   ^    " + ln
-                + " /   \\ " + ln
+                + " /   \\  " + ln
                 + "/_____\\" + ln;
     }
 }
