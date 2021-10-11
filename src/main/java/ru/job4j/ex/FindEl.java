26eaa8d12681bacf1eb5d2de1b659490f42241f2
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-10-11 11:33:14 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i< value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else {
+                throw new ElementNotFoundException("Element not found");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] mass = {"test", "rest", "chess"};
+        String key = "dest";
+        try {
+            indexOf(mass, key);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
