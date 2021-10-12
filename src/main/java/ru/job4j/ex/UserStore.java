80aa5e512cdc071871d041968a166b6a94a23bb6
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-10-12 11:46:51 +0300

    0.5. Иерархия исключений и множественный catch. [#219368][2]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -3,40 +3,40 @@ package ru.job4j.ex;
 public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        User user = new User(null, false);
-        for (int i = 0; i < users.length; i++ ) {
-            if (login.equals(users[i].getUsername())) {
-                user = users[i];
+        User result = null;
+        for (User value : users) {
+            if (login.equals(value.getUsername())) {
+                result = value;
+                break;
             }
         }
-        if(user.getUsername() == null) {
+        if(result == null) {
             throw new UserNotFoundException("User not found.");
         }
-        return user;
+        return result;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        boolean result = false;
-        if (user.getUsername().length() > 3 && user.isValid()) {
-            result = true;
-        } else if (!user.isValid()){
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User is not valid.");
         }
-        return result;
+        return true;
     }
 
     public static void main(String[] args) throws UserNotFoundException {
         User[] users = {
                 new User("Petr", false)
         };
-
-        User user = findUser(users, "Petr");
         try {
+            User user = findUser(users, "Petr");
             if (user.getUsername() != null) {
                 validate(user);
+
             }
-        } catch (Exception in) {
-            in.printStackTrace();
+        } catch (UserInvalidException uie) {
+            uie.printStackTrace();
+        } catch (UserNotFoundException unfe) {
+            unfe.printStackTrace();
         }
     }
 }
