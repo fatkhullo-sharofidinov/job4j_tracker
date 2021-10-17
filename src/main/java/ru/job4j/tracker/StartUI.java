42b8521e05bfd04e17afea65621e522f701b1da6
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-10-17 19:33:36 +0300

    9.1 Зависимость от System.out [#33568 #214045][2]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -21,12 +21,14 @@ public class StartUI {
         }
     }
 
+
     private void showMenu(UserAction[] actions) {
         out.println("Menu:");
         for (int i = 0; i < actions.length; i++) {
             out.println(i + ". " + actions[i].name());
         }
     }
+
 
     public static void main(String[] args) {
             Output output = new ConsoleOutput();
