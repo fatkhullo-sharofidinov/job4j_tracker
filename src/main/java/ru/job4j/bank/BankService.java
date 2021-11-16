01700cc4daaecf43f8aaf693071e274e2b7b547d
fatkhullo-sharofidinov <fatkhullo0998@gmail.com>
2021-11-16 16:16:22 +0300

    переводы

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,39 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (!users.containsKey(user)) {
+            users.put(user, new ArrayList<Account>());
+        }
+    }
+
+    public void addAccount(String passport, Account account) {
+    }
+
+    public User findByPassport(String passport) {
+        User result = null;
+        for (User user : users.keySet()) {
+            if (user.getPassport(passport).equals(passport)) {
+                result = user;
+            }
+        }
+        return result;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
